74a5fc6dfcd9053afb9df8e497c86f5a1c918ffc
RaflyAzharr <117628892+RaflyAzharr@users.noreply.github.com>
2023-01-26 15:23:05 +0700

    Add files via upload

---
+++ src/components/Hero.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Selamat Datang di Website Resmi</strong>
+                <br />RA ASY SYIFA
+              </h2>
+              <p>ayo daftarkan segera</p>
+              <router-link class="btn btn-lg btn-primary" to="/daftar">
+                <b-icon-arrow-right></b-icon-arrow-right>Daftar Sekarang
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption=""
+      img-src="https://scontent.fbdo7-1.fna.fbcdn.net/v/t1.6435-9/64679346_154447232354178_29709225089302528_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEJb8eM8OQJ2_cyFpPJXQ-IHe0kAN92Lkkd7SQA33YuSQdNmMHDgkRiJBJYi6_WcetGaLlx7niVTdsdpkTUcpkR&_nc_ohc=yKFplFFlvH0AX8-8CpA&tn=pMBrz_wFRteS98uN&_nc_ht=scontent.fbdo7-1.fna&oh=00_AfAaU6iIz92i5RXyjvMP1Vnn_g6uSegNaS5WS3GFRCo48w&oe=63F584D7"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src="https://scontent.fbdo7-1.fna.fbcdn.net/v/t1.6435-9/67313738_161480958317472_4205081352845918208_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFv2PtdT2gau1T-xQ4yJnqvTmg738M-0s5OaDvfwz7SziByOxdpSma3-AxOc5i1cSHwHqVIi6UXX3tRV9y4JgIe&_nc_ohc=hvpcNu37keQAX-InVtu&_nc_ht=scontent.fbdo7-1.fna&oh=00_AfDxEveDBx1wfPb5HMoIZEyyPuttxxb7dzXydHdX1lxeRg&oe=63F594A1"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src="https://scontent.fbdo7-1.fna.fbcdn.net/v/t1.6435-9/51026306_101033047695597_5299544882220630016_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHcGBn11gKRE4dxx6enFZkGDwpAl85uW6sPCkCXzm5bq2G_U9bcjNRKYWVQxG1xILwImcC6sdkA1gi0qt8gSARW&_nc_ohc=VV3SZUFRaLcAX80BOII&tn=pMBrz_wFRteS98uN&_nc_ht=scontent.fbdo7-1.fna&oh=00_AfBpj77KI2kJzMiePmYZG8pRKKuBeJ6VLp5y4Y8CT6xHtg&oe=63F57A43"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption=""
+      img-src="https://scontent.fbdo7-1.fna.fbcdn.net/v/t1.6435-9/64679346_154447232354178_29709225089302528_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEJb8eM8OQJ2_cyFpPJXQ-IHe0kAN92Lkkd7SQA33YuSQdNmMHDgkRiJBJYi6_WcetGaLlx7niVTdsdpkTUcpkR&_nc_ohc=yKFplFFlvH0AX8-8CpA&tn=pMBrz_wFRteS98uN&_nc_ht=scontent.fbdo7-1.fna&oh=00_AfAaU6iIz92i5RXyjvMP1Vnn_g6uSegNaS5WS3GFRCo48w&oe=63F584D7"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src="https://scontent.fbdo7-1.fna.fbcdn.net/v/t1.6435-9/67313738_161480958317472_4205081352845918208_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFv2PtdT2gau1T-xQ4yJnqvTmg738M-0s5OaDvfwz7SziByOxdpSma3-AxOc5i1cSHwHqVIi6UXX3tRV9y4JgIe&_nc_ohc=hvpcNu37keQAX-InVtu&_nc_ht=scontent.fbdo7-1.fna&oh=00_AfDxEveDBx1wfPb5HMoIZEyyPuttxxb7dzXydHdX1lxeRg&oe=63F594A1"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Third Slide"
+      img-src="https://scontent.fbdo7-1.fna.fbcdn.net/v/t1.6435-9/51026306_101033047695597_5299544882220630016_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHcGBn11gKRE4dxx6enFZkGDwpAl85uW6sPCkCXzm5bq2G_U9bcjNRKYWVQxG1xILwImcC6sdkA1gi0qt8gSARW&_nc_ohc=VV3SZUFRaLcAX80BOII&tn=pMBrz_wFRteS98uN&_nc_ht=scontent.fbdo7-1.fna&oh=00_AfBpj77KI2kJzMiePmYZG8pRKKuBeJ6VLp5y4Y8CT6xHtg&oe=63F57A43"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Selamat Datang di Website Resmi</strong>
+                <br />RA ASY SYIFA
+              </h2>
+              <p>ayo daftarkan segera</p>
+              <router-link class="btn btn-lg btn-primary" to="/daftar">
+                <b-icon-arrow-right></b-icon-arrow-right>Daftar Sekarang
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+body {
+  background-image: url('../assets/images/backround\ tk.jpg');
+}
+</style>
---
+++ src/views/Daftar.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col">
+          <h2>
+            Daftar
+            <strong>Peserta Didik</strong>
+          </h2>
+       <h5><b-form-group
+        id="input-group-1"
+        label="No Induk:"
+        label-for="input-0"
+       >
+       <b-form-input
+          id="input-1"
+          v-model="form.induk"
+          type="name"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+       </h5>
+        </div>
+      </div>
+  <div>
+    <br>
+    <br>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Nama Lengkap:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          type="name"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+        <br>
+      <b-form-group id="input-group-2" label="Jenis Kelamin:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.jk"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+      <br>
+      <b-form-group id="input-group-3" label="Nama Ayah:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.ayah"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+      <br>
+      <b-form-group id="input-group-4" label="Nama Ibu:" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="form.ibu"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+      <br>
+      <router-link to="datadaftar" class="btn btn-primary me-2" type="submit" variant="primary">Daftar</router-link>
+      <b-button type="reset" variant="danger">Batal</b-button>
+    </b-form>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+  export default {
+  name: "Daftar",
+  components: {
+    Navbar,
+  },
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: []
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
